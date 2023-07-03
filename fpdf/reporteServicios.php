d709c1357a2c5fda320294d8f97977d94b36a570
jmChavarria <marlon05chavarria@gmail.com>
2023-07-03 12:14:54 -0500

    cambios

--- fpdf/reporteServicios.php
+++ fpdf/reporteServicios.php
@@ -12,42 +12,20 @@ class PDF extends FPDF
 
       // $consulta_info = $conexion->query(" select * from servicios ");//traemos datos de la empresa desde BD
       // $dato_info = $consulta_info->fetch_object();
-      $this->Image('logo.png', 185, 5, 20); //logo de la empresa,moverDerecha,moverAbajo,tamañoIMG
+      $this->Image('motorbike.png', 5, -7, 50); //logo de la empresa,moverDerecha,moverAbajo,tamañoIMG
       $this->SetFont('Arial', 'B', 19); //tipo fuente, negrita(B-I-U-BIU), tamañoTexto
       $this->Cell(45); // Movernos a la derecha
       $this->SetTextColor(0, 0, 0); //color
       //creamos una celda o fila
       $this->Cell(110, 15, utf8_decode('MOTO RACER LA 36 '), 1, 1, 'C', 0); // AnchoCelda,AltoCelda,titulo,borde(1-0),saltoLinea(1-0),posicion(L-C-R),ColorFondo(1-0)
-      $this->Ln(3); // Salto de línea 
+      $this->Ln(25); // Salto de línea 
       $this->SetTextColor(103); //color
 
-      /* UBICACION */
-      $this->Cell(110);  // mover a la derecha
-      $this->SetFont('Arial', 'B', 10);
-      $this->Cell(96, 10, utf8_decode("Ubicación : "), 0, 0, '', 0);
-      $this->Ln(5);
 
-      /* TELEFONO */
-      $this->Cell(110);  // mover a la derecha
-      $this->SetFont('Arial', 'B', 10);
-      $this->Cell(59, 10, utf8_decode("Teléfono : "), 0, 0, '', 0);
-      $this->Ln(5);
-
-      /* COREEO */
-      $this->Cell(110);  // mover a la derecha
-      $this->SetFont('Arial', 'B', 10);
-      $this->Cell(85, 10, utf8_decode("Correo : "), 0, 0, '', 0);
-      $this->Ln(5);
-
-      /* TELEFONO */
-      $this->Cell(110);  // mover a la derecha
-      $this->SetFont('Arial', 'B', 10);
-      $this->Cell(85, 10, utf8_decode("Sucursal : "), 0, 0, '', 0);
-      $this->Ln(10);
 
       /* TITULO DE LA TABLA */
       //color
-      $this->SetTextColor(228, 100, 0);
+      $this->SetTextColor(0, 0, 0);
       $this->Cell(50); // mover a la derecha
       $this->SetFont('Arial', 'B', 15);
       $this->Cell(100, 10, utf8_decode("REPORTE DE SERVICIOS"), 0, 1, 'C', 0);
@@ -117,4 +95,4 @@ while ($datos_reporte = $consulta_reporte_alquiler->fetch_object()) {
 
 
 
-$pdf->Output('Prueba.pdf', 'I');//nombreDescarga, Visor(I->visualizar - D->descargar)
+$pdf->Output('reporteServicios.pdf', 'I');//nombreDescarga, Visor(I->visualizar - D->descargar)
